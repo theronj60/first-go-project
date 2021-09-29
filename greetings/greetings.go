c71eb970019339e54a69615454465c97965abda2
Theron Joe <theronj@joetwebdev.io>
2021-09-28 23:25:50 -0600

    added testing files and finished starter project.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -10,10 +10,29 @@ import (
 func Hello(name string) (string, error) {
 	//If no name was given, return an error with a message.
 	if name == "" {
-		return "", errors.New("empty name")
+		return name, errors.New("empty name")
 	}
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+//Hellos returns a map that associates each of the named people
+//with a gretting messages.
+
+func Hellos(names []string) (map[string]string, error) {
+	// a map to associate names with messages.
+	messages := make(map[string]string)
+	// loop through the received slice of names, calling
+	// the hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		// in the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
 }
 
 // init sets initial values for variables used in the function
